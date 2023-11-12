010d5b9e9e41deeda2ad4c6c116a328af1051e3e
Eduardo Dalla Vecchia <eduardo@MacBook-Pro-de-Eduardo.local>
2023-11-12 13:31:06 -0300

    Sidebar menu done

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -4,14 +4,16 @@
     <q-drawer
       v-model="leftDrawerOpen"
       show-if-above
-      bordered
+      class="sidebar-menu"
+      :width="200"
     >
-      <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
+      <q-list bordered separator>
+        <span class="title-menu text-center">MENU</span>
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
       </q-list>
     </q-drawer>
 
@@ -25,9 +27,46 @@
 import { ref } from 'vue';
 import HeaderComponent from 'components/HeaderComponent.vue';
 
+import EssentialLink, { EssentialLinkProps } from 'components/EssentialLink.vue';
+
+const essentialLinks: EssentialLinkProps[] = [
+  {
+    title: 'Pessoas',
+    icon: 'person_add',
+    link: '/'
+  },
+  {
+    title: 'Pedidos',
+    icon: 'grading',
+    link: '/pedidos'
+  }
+];
+
 const leftDrawerOpen = ref(false)
 
 function toggleLeftDrawer() {
   leftDrawerOpen.value = !leftDrawerOpen.value
 }
 </script>
+
+<style lang="scss">
+  .sidebar-menu {
+    background-color: darken($primary, 7%);
+    border: 0;
+
+    .title-menu {
+      color: #fff;
+      font-weight: bold;
+      display: block;
+      padding: 13.5px;
+    }
+
+    .q-item {
+      color: #fff;
+
+      &.q-router-link--exact-active:not(:hover) {
+        background-color: darken($primary, 13%);
+      }
+    }
+  }
+</style>
